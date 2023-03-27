9f47d7b86594633fde50bc45339d1b8938d38705
ustechacademy <ustechacademytool@gmail.com>
2023-03-27 23:59:29 +0200

    Tenzies app

--- lesson8/tenzies/src/components/Die.js
+++ lesson8/tenzies/src/components/Die.js
@@ -2,12 +2,27 @@ import React from "react"
 
 
 export default function Die(props){
-    console.log(props);
+
+    function dieSelected(die){
+
+        console.log("die selected",props.index);
+
+        props.setDice((oldDice) => {
+            const newDice = [...oldDice]; // Güncel statedeki zar objelerini kopyaladik.
+            newDice[props.index] = {value: newDice[props.index].value, 
+                isSelected: !newDice[props.index].isSelected}
+            return newDice;
+        }
+        )
+
+
+    }
     return(
         <>
         <div 
             className="die-face"
             style={{backgroundColor: props.dieObj.isSelected ? "#59E391" : "white"}}
+            onClick={() => dieSelected(props.dieObj)}
         >
             <h3 className="die-num">{props.dieObj.value}</h3>
         </div>
